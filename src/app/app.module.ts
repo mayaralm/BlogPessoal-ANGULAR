ebce12403cd5bebc30588e1b1204fbe538c5158d
mayaralm <mmayaraleitedemelo@gmail.com>
2021-12-10 10:33:46 -0300

    criacão de todo o componente de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy,LocationStrategy } from '@angular/common'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,11 @@ import { InicioComponent } from './inicio/inicio.component';
     FormsModule
 
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
